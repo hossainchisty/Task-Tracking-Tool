1de05000a1fe859e4c901af9d38a6bf2c3f7b85b
hossainchisty <hossain.chisty11@gmail.com>
2023-03-20 14:37:38 +0600

    🗃️ Added new model

---
+++ models/otpModel.js
@@ -0,0 +1,24 @@
+// Basic Lib Imports
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OTP = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    OTP: {
+      type: String,
+      required: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model("OTP", OTP);
