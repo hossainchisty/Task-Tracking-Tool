4edd8ae65fb7721f272baa5a136e5db5bb811af1
hossainchisty <hossain.chisty11@gmail.com>
2023-05-07 23:38:29 +0600

    feat: Added corn job for reminder

---
+++ service/cron.js
@@ -0,0 +1,23 @@
+const cron = require("node-cron");
+
+/**
+ * @desc    Schedules a cron job to run every minute with the provided title.
+ * @param {string} title - The title of the job to be displayed in the console log.
+ */ 
+
+const scheduleCronJob = (title) => {
+  cron.schedule(
+    "*/1 * * * *",
+    function () {
+      console.log(`Hello, you have remain task to do - ${title}`);
+    },
+    {
+      timeZone: "Asia/Dhaka",
+      scheduled: true,
+    }
+  );
+
+  isCronJobScheduled = true;
+};
+
+module.exports = scheduleCronJob;
