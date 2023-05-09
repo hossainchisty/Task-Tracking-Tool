123768408a457fdbe7b38e14e6a2a0a877ef9d93
hossainchisty <hossain.chisty11@gmail.com>
2023-05-09 15:40:11 +0600

    feat: Added comments model

---
+++ models/commentModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+/*
+
+Schema for comments on tasks
+task: reference to task
+author: reference to user
+text: text of comment
+timestamps: automatically add timestamps for when the comment was created and when it was last updated
+versionKey: false: don't add __v field to the output of the document
+
+*/
+
+const commentSchema = mongoose.Schema(
+  {
+    task: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
