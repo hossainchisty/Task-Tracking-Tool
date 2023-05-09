1e660637c3d113fcf31209a569645c4e3c216d99
hossainchisty <hossain.chisty11@gmail.com>
2023-05-09 21:23:16 +0600

    feat(package): Adding a new package to package.json

--- routes/taskRouters.js
+++ routes/taskRouters.js
@@ -11,6 +11,13 @@ const {
 } = require("../controllers/taskController");
 
 const {
+  getComment,
+  addComment,
+  // updateComment,
+  deleteComment
+} = require("../controllers/commentController");
+
+const {
   addCollaborator,
   removeCollaborator,
 } = require("../controllers/collaboratorController");
@@ -26,7 +33,16 @@ router.route("/:id").delete(protect, deleteTask).put(protect, updateTask);
 router.route("/assigned").get(protect, getassignedTasks);
 
 // Adding Collaborators
-router.route("/:taskId/collaborators/:userId").post(protect, addCollaborator);
+router.route("/:taskId/collaborators/").post(protect, addCollaborator);
+
+// Adding Comments
+router.route("/:taskId/comments/").post(protect, addComment);
+
+// Removing comments
+router.route("/delete/:taskId/comments/:commentId").delete(protect, deleteComment);
+
+// Get comments
+router.route("/:id/comments").get(protect, getComment);
 
 //  Removing Collaborators
 router
---
+++ service/emailService.js
@@ -0,0 +1,36 @@
+var nodemailer = require("nodemailer");
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  host: `${process.env.HOST}`,
+  port: 25,
+  secure: false,
+  auth: {
+    user: `${process.env.EMAIL}`,
+    pass: `${process.env.PASS}`,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+const sendConfirmationEmail = async (email, reservationDetails) => {
+  await transporter.sendMail({
+    from: `Reservation ${process.env.FROM}`,
+    to: email,
+    subject: "Reservation Confirmation", 
+    text: `Dear ${reservationDetails.full_name}, 
+
+    Thank you for choosing Brasa at ${reservationDetails.branch} for your reservation on ${reservationDetails.date} at ${reservationDetails.time}.
+    We are pleased to confirm your reservation for ${reservationDetails.guests} guests.
+
+    Your ${reservationDetails.promoCode} promo code has been successfully applied and your discount of [discount amount] has been deducted from your total bill.
+
+    If you need to make any changes to your reservation, please contact us at least 24 hours in advance. We look forward to serving you soon.
+  
+    Sincerely,
+    Brasa
+    `,
+  });
+};
+
+module.exports = sendConfirmationEmail;
