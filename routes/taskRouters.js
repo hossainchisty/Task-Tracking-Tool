6a9738209232092770bdbe92485f0c5d73fc59ee
hossainchisty <hossain.chisty11@gmail.com>
2023-05-09 15:30:59 +0600

    refactor: Reorganize code comments to better align with code structure and logic

--- routes/taskRouters.js
+++ routes/taskRouters.js
@@ -7,16 +7,31 @@ const {
   addTask,
   updateTask,
   deleteTask,
-  getassignedTasks
+  getassignedTasks,
 } = require("../controllers/taskController");
-const {protect} = require("../middleware/authMiddleware");
 
+const {
+  addCollaborator,
+  removeCollaborator,
+} = require("../controllers/collaboratorController");
+const { protect } = require("../middleware/authMiddleware");
 
-// Routing Implement
-router.route('/').get(protect, getTask).post(protect, addTask)
-router.route('/:id').delete(protect, deleteTask).put(protect, updateTask)
+// Adding n getting tasks
+router.route("/").get(protect, getTask).post(protect, addTask);
 
-router.route('/assigned').get(protect, getassignedTasks)
+// Deleting n updating tasks
+router.route("/:id").delete(protect, deleteTask).put(protect, updateTask);
+
+// Assigned Tasks
+router.route("/assigned").get(protect, getassignedTasks);
+
+// Adding Collaborators
+router.route("/:taskId/collaborators/:userId").post(protect, addCollaborator);
+
+//  Removing Collaborators
+router
+  .route("/delete/:taskId/collaborators/:userId")
+  .post(protect, removeCollaborator);
 
 // Exporting the Router
 module.exports = router;
