873f9bacea38cbb268a24bf43d14a2a871ac2c03
hossainchisty <hossain.chisty11@gmail.com>
2023-05-12 15:20:15 +0600

    fix: Hide version key

--- models/taskHistoryModel.js
+++ models/taskHistoryModel.js
@@ -4,26 +4,29 @@ const mongoose = require("mongoose");
 /*
     Task History Schema
 */
-const TaskHistorySchema = mongoose.Schema({
-  task: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Task",
-    required: true,
+const TaskHistorySchema = mongoose.Schema(
+  {
+    task: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    action: {
+      type: String,
+      required: true,
+      enum: ["created", "updated", "deleted", "completed", "incomplete"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  action: {
-    type: String,
-    required: true,
-    enum: ["created", "updated", "deleted", "completed", "incomplete"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("TaskHistory", TaskHistorySchema);
