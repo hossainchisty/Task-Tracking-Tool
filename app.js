31a5138d29e118e5364e92c21f0a69f0f33ae192
hossainchisty <hossain.chisty11@gmail.com>
2023-05-20 15:07:03 +0600

    fix: app.js to use good security practices, parse request bodies, and enable CORS.
    - Replace vanilla imports of dependencies with descriptive comments
    - Import helmet middleware for securing HTTP headers
    - Import body-parser middleware for parsing request bodies
    - Load environment variables from .env file
    - Import CORS middleware for enabling Cross-Origin Resource Sharing
    - Move error middleware function to own file
    - Create an instance of the Express application
    - Use Helmet middleware to secure HTTP headers
    - Parse JSON request bodies with bodyParser middleware
    - Enable CORS for all origins, methods, headers, and credentials
    - Mount routers for handling user, task, subscription, and history-related routes
    - Define a fallback route for unrecognized routes
    - Add custom error handling on top of Express's default error handling
    - Export the Express app for external use

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 // Basic Lib Imports
-const express = require("express");
-const bodyParser = require('body-parser');
-require("dotenv").config();
-var cors = require('cors')
-const { errorHandler } = require("./middleware/errorMiddleware");
+const express = require("express"); // Importing the Express framework for building web applications
+const helmet = require("helmet"); // Importing Helmet middleware for securing HTTP headers
+const bodyParser = require('body-parser'); // Importing Body Parser middleware for parsing request bodies
+require("dotenv").config(); // Loading environment variables from .env file
+const cors = require('cors'); // Importing CORS middleware for enabling Cross-Origin Resource Sharing
+const { errorHandler } = require("./middleware/errorMiddleware"); // Importing custom error handling middleware
+
 // Database connection with mongoose
-const connectDB = require("./config/db");
-connectDB();
-const port = process.env.PORT || 8000;
+const connectDB = require("./config/db"); // Importing database connection function using Mongoose
+connectDB(); // Establishing the database connection
 
-// Express app initialization
-const app = express();
-app.use(bodyParser.json());
-app.use(express.json());
-// This is CORS-enabled for all origins!
-app.use(cors())
 
+
+const app = express(); // Creating an instance of the Express application
+app.use(helmet()); // Setting up Helmet middleware for securing HTTP headers
+app.use(bodyParser.json()); // Parsing JSON bodies
+app.use(express.json()); // Parsing JSON bodies
+app.use(cors(
+  {
+    origin: "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+  }
+)); // Enabling CORS for all origins
 app.use(
   express.urlencoded({
     extended: false,
@@ -23,17 +31,17 @@ app.use(
 );
 
 // Routing Implement
-app.use("/api/v1/users", require("./routes/userRouters"));
-app.use("/api/v2/tasks", require("./routes/taskRouters"));
-app.use("/api/v2/tasks/history", require("./routes/historyRouters"));
-app.use("/api/v2/subscription", require("./routes/subscriptionRouters"));
+app.use("/api/v1/users", require("./routes/userRouters")); // Mounting userRouters for handling user-related routes
+app.use("/api/v2/tasks", require("./routes/taskRouters")); // Mounting taskRouters for handling task-related routes
+app.use("/api/v2/tasks/history", require("./routes/historyRouters")); // Mounting historyRouters for handling history-related routes
+app.use("/api/v2/subscription", require("./routes/subscriptionRouters")); // Mounting subscriptionRouters for handling subscription-related routes
 
 // Undefined Route Implement
-app.use("*",(req,res)=>{
-  res.status(404).json({status:"fail",data:"Not Found"})
-})
+app.use("*", (req, res) => {
+  res.status(404).json({ status: "fail", data: "Not Found" });
+});
 
-// Custome error handler
+// Custom error handler
 app.use(errorHandler);
 
-module.exports = app;
+module.exports = app; // Exporting the Express app for external use
